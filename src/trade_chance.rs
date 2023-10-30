9e84a4d2a2ac0145265d8cc75e04856b92ce7578
Shigeo Nakamura <snakamura@de-c-362.cinemo-de.internal>
2023-10-30 18:42:34 +0100

    Add helper functions

--- src/trade_chance.rs
+++ src/trade_chance.rs
@@ -9,6 +9,16 @@ pub enum TradeAction {
     SellClose,
 }
 
+impl TradeAction {
+    pub fn is_open(&self) -> bool {
+        matches!(self, TradeAction::BuyOpen | TradeAction::SellOpen)
+    }
+
+    pub fn is_buy(&self) -> bool {
+        matches!(self, TradeAction::BuyOpen | TradeAction::BuyClose)
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 pub struct TradeChance {
     pub trader_name: String,
@@ -17,7 +27,7 @@ pub struct TradeChance {
     pub amounts: Vec<f64>,
     pub action: TradeAction,
     pub reason_for_close: Option<ReasonForClose>,
-    pub current_price: Option<f64>,
+    pub price: Option<f64>,
     pub predicted_price: Option<f64>,
     pub atr: Option<f64>,
     pub momentum: Option<f64>,
